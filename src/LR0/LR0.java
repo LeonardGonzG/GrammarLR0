882cf73be827369a81067b94f2a5c5dbb02e1809
LeonardGonzG <gonzalezgutierrezleonardo1@gmail.com>
2020-03-10 21:59:12 -0500

    Method identify after-before point

--- src/LR0/LR0.java
+++ src/LR0/LR0.java
@@ -5,16 +5,79 @@ package LR0;
  * @author Usuario
  */
 public class LR0 {
-    
-    
-    private TableLR0 lr;
+
+    TableLR0 lr;
 
     public LR0(TableLR0 lr) {
         this.lr = lr;
     }
+
+    public void lr0(TableLR0 lr, int codPlus, int codSave, int IsSave, int IdSave, int puntero) {
+
+        String aux="";
+        
+        if (codPlus == 0) {
+
+            lr.row.add(new ProductionLR0(codPlus + 1, IsSave, -1, "*", lr.gramUser.get(0), false, false));
+            puntero = codPlus + 1;
+            
+            aux=identifyAfterPoint(lr.gramUser.get(0));
+        }
+
+    }
+
     
     
     
-    
-    
+///----------------------------------------------------------------------------
+    public String identifyAfterPoint(NTProduction prod) {
+
+        int pos = 0;
+
+        for (int i = 0; i < prod.getMyList().size(); i++) {
+
+            if (i == 0) {
+
+                return prod.getMyList().get(1);
+
+            } else if (prod.getMyList().get(i).equals(".")) {
+
+                if (i == (prod.getMyList().size() - 1)) {
+
+                    return "$";
+
+                } else {
+
+                    pos = i + 1;
+
+                    return prod.getMyList().get(pos);
+                }
+
+            }
+
+        }
+
+        return "Error identify after point";
+
+    }
+///-------------------------------------------------------------------------------------
+    public String identifyBeforePoint(NTProduction prod) {
+
+        int pos = 0;
+
+        for (int i = 0; i < prod.getMyList().size(); i++) {
+
+            if (i > 0) {
+
+                pos = i - 1;
+                return prod.getMyList().get(pos);
+
+            }
+
+        }
+
+        return "Error identify before point";
+
+    }
+
 }
--- src/LR0/TableLR0.java
+++ src/LR0/TableLR0.java
@@ -13,8 +13,8 @@ public class TableLR0 {
     
     
     private String nameTable;
-    private List<NTProduction> gramUser = new ArrayList<>();
-    private ArrayList<ProductionLR0> row= new ArrayList<>();
+     List<NTProduction> gramUser = new ArrayList<>();
+     ArrayList<ProductionLR0> row= new ArrayList<>();
 
     public TableLR0(String nameTable, List<NTProduction> mainGrammar) {
        
@@ -22,22 +22,7 @@ public class TableLR0 {
         this.gramUser= mainGrammar;
     }
 
-    public List<NTProduction> getGramUser() {
-        return gramUser;
-    }
-
-    public void setGramUser(List<NTProduction> gramUser) {
-        this.gramUser = gramUser;
-    }
-
-    public ArrayList<ProductionLR0> getRow() {
-        return row;
-    }
-
-    public void setRow(ArrayList<ProductionLR0> row) {
-        this.row = row;
-    }
-
+ 
     public String getNameTable() {
         return nameTable;
     }
